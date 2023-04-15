b1f2791c4741f1986d26160e75f6b65ea6efbe2a
Clashicism <60586534+Clashicism@users.noreply.github.com>
2023-04-15 17:49:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   });
 
-client.on('message', msg => {  console.log(`on message`);
+client.on('message', msg => {                                       console.log(`on message`);
   let crit=0; let stun=0; let high=0; let glitch=0; let reply="";
   let message=msg.content.toLowerCase();
   let words=message.split(" ");
   let [input,x]=[words[0],words[1]];
-  if (input === 'roll') {
+  if (input === 'roll') {                                       console.log(`on a roll`);
     let dieRoll=0; let amount=0; let diceText=""; 
     if(x>0) {
       for(i=0; i<x; i++) { 
@@ -34,25 +34,25 @@ client.on('message', msg => {  console.log(`on message`);
         if(dieRoll==5) { stun+=1; } 
         if((dieRoll==1)||(dieRoll==2)) { glitch+=1; } }
       reply=diceText+" Total: "+amount + " High: "+high;
-      if(high>5) {
+      if(high>5) {                                       console.log(`high five`);
         if(crit>3) { reply+=", Annihilation! "+crit+ " critical successes"; }
         else if(crit==3) { reply+=", triple critical"; }
         else if(crit==2) { reply+=", double critical"; }
         else if(crit==1) { reply+=", critical"; } }
-      if(high>4) {
+      if(high>4) {                                       console.log(`low five`);
         if(stun>0) { reply+=", "+stun+" stun"; }
         if(glitch >= (x/2)) { reply+=", glitch"; } }
-      else if(high>2) { 
+      else if(high>2) {                                  console.log(`two timer`);
         if(glitch >= (x/2)) { reply+=", critical fail"; } 
         else { reply+=", miss"; } }
       else if(high>1) { reply+=", critical fumble"; }
       else { reply+=", epic fail"; } 
       x=0; input=""; msg.reply(reply); }
     else { reply="I want to play!!!"; x=0; input=""; msg.reply(reply); } }
-  if (input === 'soft') {
+  if (input === 'soft') {                                console.log(`gone soft`);
 
     let dieRoll=0; let amount=0; let diceText="";
-    if(x>0) {
+    if(x>0) {                                            console.log(`got dice?`);
       for(i=0; i<x; i++) { 
         dieRoll=parseInt((Math.random() * 6)+1);
         diceText+=dice[dieRoll]+" "; amount+=dieRoll; 
@@ -61,17 +61,17 @@ client.on('message', msg => {  console.log(`on message`);
         if(dieRoll==5) { stun+=1; } 
         if((dieRoll==1)||(dieRoll==2)) { glitch+=1; } }
       reply=diceText+" Total: "+amount + " High: "+high;
-      if(high>4) {
+      if(high>4) {                                       console.log(`#5 is alive`);
         if(stun>0) { reply+=", "+stun+" stun"; }
         if(glitch >= (x/2)) { reply+=", glitch"; } }
-      else if(high>2) { 
+      else if(high>2) {                                  console.log(`3rd time's the charm`);
         if(glitch >= (x/2)) { reply+=", critical fail"; } 
         else { reply+=", miss"; } }
       else if(high>1) { reply+=", critical fumble"; }
       else { reply+=", epic fail"; }
       x=0; input=""; msg.reply(reply); }
     else { reply="I want to dance!"; x=0; input=""; msg.reply(reply); } }
-  if (input === 'talk') {
+  if (input === 'talk') {                                console.log(`talk is cheap`);
     reply= "Hi, I'm "+client.user.tag; 
     if (x) { words.shift(); 
        if(words[0]==="roll") { reply+="\n Type `roll` and a number, like `roll 2`. Try it." }
@@ -79,10 +79,9 @@ client.on('message', msg => {  console.log(`on message`);
     msg.reply(reply); }
   else if (input === 'ping') { msg.reply('pong'); }
   else if (input === 'data') { msg.reply("msg.author.id "+msg.author.id+"\n"); }
-  else if (input === 'user') { 
+  else if (input === 'user') {                           console.log(`user input`);
     reply="Collecting data \n";
-    reply+="msg.author.id "+msg.author.id+"\n";
-    console.log(reply);
+    reply+="msg.author.id "+msg.author.id+"\n";          console.log(reply);
     msg.reply(reply); 
 // webhookClient.send(reply);
   }
